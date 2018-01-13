165f31229d47f40aabdd00b7fa332917ae67dbcb
Jacek Kopecky <jacek@jacek.cz>
2018-01-13 14:57:12 +0000

    :art: consistent indent

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const treatmentHistoryLength = 10;
 const vetMaximumStay = 5000;
 
 const cats = [
-    { name: 'Claude', pic: 'i/1.png' },
-    { name: 'Colin', pic: 'i/2.png' },
-    { name: 'Fluffy', pic: 'i/3.png' },
-    { name: 'Ghengis', pic: 'i/4.png' },
-    { name: 'Madge', pic: 'i/5.png' },
-    { name: 'Penny', pic: 'i/6.png' },
-    { name: 'Roger', pic: 'i/7.png' },
-    { name: 'Top', pic: 'i/8.png' },
+  { name: 'Claude', pic: 'i/1.png' },
+  { name: 'Colin', pic: 'i/2.png' },
+  { name: 'Fluffy', pic: 'i/3.png' },
+  { name: 'Ghengis', pic: 'i/4.png' },
+  { name: 'Madge', pic: 'i/5.png' },
+  { name: 'Penny', pic: 'i/6.png' },
+  { name: 'Roger', pic: 'i/7.png' },
+  { name: 'Top', pic: 'i/8.png' },
 ];
 
 let box = null;
@@ -21,44 +21,44 @@ let dragParent = null;
  * @param {Event} e is a drag event
  */
 function catDragStarted(e) {
-    let sendThisWithTheDrag = e.target.dataset.cat;
-    e.dataTransfer.setData('application/json', sendThisWithTheDrag);
-    e.dataTransfer.setDragImage(box, 100, 40);
-    e.dataTransfer.effectAllowed = 'move';
-    dragParent = e.target.parentElement;
+  let sendThisWithTheDrag = e.target.dataset.cat;
+  e.dataTransfer.setData('application/json', sendThisWithTheDrag);
+  e.dataTransfer.setDragImage(box, 100, 40);
+  e.dataTransfer.effectAllowed = 'move';
+  dragParent = e.target.parentElement;
 }
 
 /**
  * @param  {object} cat is an object describing a dragged cat
  */
 function updateTreatmentHistory(cat) {
-    let treated = [];
-    if (localStorage.treated) {
-        treated = JSON.parse(localStorage.treated);
-    }
-    if (cat) {
-        treated.push(cat.name);
-    }
-    localStorage.treated = JSON.stringify(treated);
-    window.log.textContent = `The last ${treatmentHistoryLength} cats to be treated were: ${ treated.slice(-treatmentHistoryLength).join(', ')}`;
+  let treated = [];
+  if (localStorage.treated) {
+    treated = JSON.parse(localStorage.treated);
+  }
+  if (cat) {
+    treated.push(cat.name);
+  }
+  localStorage.treated = JSON.stringify(treated);
+  window.log.textContent = `The last ${treatmentHistoryLength} cats to be treated were: ${ treated.slice(-treatmentHistoryLength).join(', ')}`;
 }
 
 /**
  * @param {Event} e is a drag event fired when a cat is droppped on a UI element
  */
 function catDropped(e) {
-    let received = e.dataTransfer.getData('application/json');
-    if (received) {
-      e.preventDefault();
-      let cat = JSON.parse(received);
-      let elem = document.getElementById(cat.id);
-      elem.dataset.checkInTime = Date.now();
-      e.currentTarget.appendChild(elem);
+  let received = e.dataTransfer.getData('application/json');
+  if (received) {
+    e.preventDefault();
+    let cat = JSON.parse(received);
+    let elem = document.getElementById(cat.id);
+    elem.dataset.checkInTime = Date.now();
+    e.currentTarget.appendChild(elem);
 
-      if (e.currentTarget === window.vet) {
-          updateTreatmentHistory(cat);
-      }
+    if (e.currentTarget === window.vet) {
+      updateTreatmentHistory(cat);
     }
+  }
 }
 
 /**
@@ -77,26 +77,26 @@ function dragHandler(e) {
  * @param {number} catIdx unique number of the cat
  */
 function addCat(cat, catIdx) {
-    const kitty = document.createElement('figure');
-    const pic = document.createElement('img');
-    const nom = document.createElement('figcaption');
+  const kitty = document.createElement('figure');
+  const pic = document.createElement('img');
+  const nom = document.createElement('figcaption');
 
-    cat.id = `cat${catIdx}`;
-    kitty.id = cat.id;
-    kitty.className = 'cat';
-    kitty.draggable = true;
-    kitty.dataset.cat = JSON.stringify(cat);
-    kitty.addEventListener('dragstart', catDragStarted);
+  cat.id = `cat${catIdx}`;
+  kitty.id = cat.id;
+  kitty.className = 'cat';
+  kitty.draggable = true;
+  kitty.dataset.cat = JSON.stringify(cat);
+  kitty.addEventListener('dragstart', catDragStarted);
 
-    nom.textContent = cat.name;
+  nom.textContent = cat.name;
 
-    pic.src = cat.pic;
-    pic.alt = 'A cat';
-    pic.draggable = false;
+  pic.src = cat.pic;
+  pic.alt = 'A cat';
+  pic.draggable = false;
 
-    kitty.appendChild(pic);
-    kitty.appendChild(nom);
-    window.clowder.appendChild(kitty);
+  kitty.appendChild(pic);
+  kitty.appendChild(nom);
+  window.clowder.appendChild(kitty);
 }
 
 
@@ -119,23 +119,23 @@ function checkOut() {
  * Start up the app.
  */
 function boot() {
-    // inject kitten data into document as HTML
-    cats.forEach(addCat);
+  // inject kitten data into document as HTML
+  cats.forEach(addCat);
 
-    // show which cats have been treated
-    updateTreatmentHistory();
+  // show which cats have been treated
+  updateTreatmentHistory();
 
-    // prepare box image for use when dragging cats
-    box = document.createElement('img');
-    box.src = 'i/carrier.png';
-    box.width = 10;
+  // prepare box image for use when dragging cats
+  box = document.createElement('img');
+  box.src = 'i/carrier.png';
+  box.width = 10;
 
-    window.vet.addEventListener('drop', catDropped);
-    window.vet.addEventListener('dragover', dragHandler);
-    window.clowder.addEventListener('drop', catDropped);
-    window.clowder.addEventListener('dragover', dragHandler);
+  window.vet.addEventListener('drop', catDropped);
+  window.vet.addEventListener('dragover', dragHandler);
+  window.clowder.addEventListener('drop', catDropped);
+  window.clowder.addEventListener('dragover', dragHandler);
 
-    window.setInterval(checkOut, 1000);
+  window.setInterval(checkOut, 1000);
 }
 
 window.addEventListener('load', boot);
